8ad40aa2a86a708813f8c81e1aa561af6add45fa
Harald Friessnegger <harald@webmeisterei.com>
2010-08-16 08:38:39 +0000

    play well with zest.releaser again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = '1.0a3'
+version = '1.0a4dev'
 readme = open("README.txt").read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
